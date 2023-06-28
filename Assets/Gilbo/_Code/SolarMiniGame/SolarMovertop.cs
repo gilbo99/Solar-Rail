f38d35c4f16893f8b720c990bf170288589c9888
Gilbo <Andrew.gilbert@y7mail.com>
2023-06-28 14:03:29 +1000

    Fixed SolarPanel

--- Assets/Gilbo/_Code/SolarMiniGame/SolarMovertop.cs
+++ Assets/Gilbo/_Code/SolarMiniGame/SolarMovertop.cs
@@ -4,98 +4,42 @@ using UnityEngine;
 
 public class SolarMovertop : MonoBehaviour
 {
-    public float rotateSpeed;
+    public float rotateSpeed = 0.5f;
     public float maxRotation = -30;
     public float minRotation = -150;
-    public float BatteryCharge;
-    public int RandomNumber;
     public float RotationX;
     public GameObject gameM;
-    //public GameObject Sun;
-    
-    //int Count;
 
-
-    // Update is called once per frame
     private Quaternion camRotation;
+    // Start is called before the first frame update
     void Start()
     {
         camRotation = transform.localRotation;
-        
-
     }
-    
 
+    // Update is called once per frame
     void Update()
     {
-
         //Movement
         if (Input.GetKey("a"))
         {
             camRotation.x += rotateSpeed;
-            print("A");
+            print("a");
+            RotationX = camRotation.x;
+            gameM.GetComponent<SolarManager>().SetRotationx(RotationX);
         }
 
         if (Input.GetKey("d"))
         {
             camRotation.x -= rotateSpeed;
-            print("d");
+            print("D");
+            RotationX = camRotation.x;
+            gameM.GetComponent<SolarManager>().SetRotationx(RotationX);
         }
-/*
-            //Makes it that the Solar panel cant go past a certain Z/X
-            if (camRotation.z > maxRotation - 5)
-                camRotation.x -= rotateSpeed;
-                RotationX = camRotation.x;
-                gameM.GetComponent<SolarManager>().SetRotationx(RotationX);
-
-            if (camRotation.z < minRotation + 5)
-                camRotation.x += rotateSpeed;
-         */
-            //makes it move
-            camRotation.x = Mathf.Clamp(camRotation.z, minRotation, maxRotation);
-            transform.localRotation = Quaternion.Euler(camRotation.x, 0, 0);
-        // Shows the X rotation for the solar panel
-        //
 
 
-
-
-        // spawns sun and moves it
-
-
-        // Sun.transform.position = new Vector3(0, 0, 2);
-
-
-
-
-
-       // print(Sun.transform.position);
-
-
+        camRotation.x = Mathf.Clamp(camRotation.x, minRotation, maxRotation);
+        transform.localRotation = Quaternion.Euler(camRotation.x, 0, 0);
 
     }
-
-    
-
-    /*
-    void FixedUpdate()
-    {
-        
-        if (Count > 15)
-        {
-            Debug.Log(camRotation.x);
-            SolarAngle = camRotation.x;
-            Count = 0;
-        }
-        Count++;
-        print(Count);
-
-
-    }
-
-    */
-
-
-
-
 }
