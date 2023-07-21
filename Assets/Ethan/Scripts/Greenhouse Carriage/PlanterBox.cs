1442a254bb440fba411bfc2078351bc0eaa56b44
Ethan <Ethan-GameDevelopment@users.noreply.github.com>
2023-07-21 14:03:38 +1000

    Ethan - Greenhouse Carriage - Added Initial Assets

---
+++ Assets/Ethan/Scripts/Greenhouse Carriage/PlanterBox.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanterBox : MonoBehaviour
+{
+
+    // PLANTER ID
+
+    // VARIABLE FOR TRACKING PLANTER INDIVIDUAL WATER
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WHEN TRIGGER ENTER WATER FLOW ADD WATER LEVEL
+
+        // WHEN LEAVE STOP ADDING WATER LEVEL
+
+        //
+    }
+}
